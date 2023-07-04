3ccf496d4ee0c959c822f4c341ddb48520421f05
Dimash <dinmukhamed.amirov@gmail.com>
2023-07-05 00:08:46 +0600

    add block promotion-lottery

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ document.querySelector(".burger").addEventListener("click", function () {
   this.classList.toggle("active");
   document.querySelector(".nav").classList.toggle("open");
 });
+
+const onlineTicket = document.querySelector(".button__online-ticket");
+const paperTicket = document.querySelector(".button__paper-ticket");
+const iFrame = document.querySelector(".iframe");
+
+onlineTicket.addEventListener("click", () => {
+  iFrame.src = "./online-ticket.html";
+  iFrame.height = "580";
+  onlineTicket.classList.remove("button-blue");
+  onlineTicket.classList.add("button-white");
+  paperTicket.classList.add("button-blue");
+});
+
+paperTicket.addEventListener("click", () => {
+  iFrame.src = "./paper-ticket.html";
+  iFrame.height = "970";
+  paperTicket.classList.remove("button-blue");
+  paperTicket.classList.add("button-white");
+  onlineTicket.classList.add("button-blue");
+});
