d886d1066f0c2b3e532c4e23122a1e4a9220c4df
Dimash <dinmukhamed.amirov@gmail.com>
2023-07-06 16:02:59 +0600

    add draw-dates section

--- script.js
+++ script.js
@@ -1,24 +1,74 @@
-document.querySelector(".burger").addEventListener("click", function () {
-  this.classList.toggle("active");
-  document.querySelector(".nav").classList.toggle("open");
-});
+////////////////////////////////// * Burger menu
 
-const onlineTicket = document.querySelector(".button__online-ticket");
-const paperTicket = document.querySelector(".button__paper-ticket");
-const iFrame = document.querySelector(".iframe");
+const openCloseBurgerMenu = () => {
+  const burgerMenu = document.querySelector(".burger");
+  const navbar = document.querySelector(".nav");
 
-onlineTicket.addEventListener("click", () => {
-  iFrame.src = "./online-ticket.html";
-  iFrame.height = "580";
-  onlineTicket.classList.remove("button-blue");
-  onlineTicket.classList.add("button-white");
-  paperTicket.classList.add("button-blue");
-});
+  burgerMenu.addEventListener("click", () => {
+    this.classList.toggle("active");
+    navbar.classList.toggle("open");
+  });
+};
+openCloseBurgerMenu();
 
-paperTicket.addEventListener("click", () => {
-  iFrame.src = "./paper-ticket.html";
-  iFrame.height = "970";
-  paperTicket.classList.remove("button-blue");
-  paperTicket.classList.add("button-white");
-  onlineTicket.classList.add("button-blue");
-});
+//////////////////////////////// *iframe
+
+const changeIframe = () => {
+  const onlineTicket = document.querySelector(".button__online-ticket");
+  const paperTicket = document.querySelector(".button__paper-ticket");
+  const iFrame = document.querySelector(".iframe");
+
+  onlineTicket.addEventListener("click", () => {
+    iFrame.src = "./online-ticket.html";
+    iFrame.height = "580";
+    onlineTicket.classList.remove("button-blue");
+    onlineTicket.classList.add("button-white");
+    paperTicket.classList.add("button-blue");
+  });
+
+  paperTicket.addEventListener("click", () => {
+    iFrame.src = "./paper-ticket.html";
+    iFrame.height = "970";
+    paperTicket.classList.remove("button-blue");
+    paperTicket.classList.add("button-white");
+    onlineTicket.classList.add("button-blue");
+  });
+};
+changeIframe();
+
+////////////////////////////////////////// * Accordion
+
+const openPanel = () => {
+  let acc = document.getElementsByClassName("accordion");
+  let i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+};
+openPanel();
+
+////////////////////////////////////////// * Check-registration
+
+const showCheckRegistration = () => {
+  const onlineTicket = document.querySelector(".button__online-ticket");
+  const paperTicket = document.querySelector(".button__paper-ticket");
+  const checkRegistration = document.querySelector(".check-registration");
+
+  paperTicket.addEventListener("click", () => {
+    checkRegistration.style.display = "flex";
+  });
+
+  onlineTicket.addEventListener("click", () => {
+    checkRegistration.style.display = "none";
+  });
+};
+showCheckRegistration();
